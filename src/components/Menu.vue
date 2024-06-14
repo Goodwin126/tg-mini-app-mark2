3bdc05b7147b95e0c0d4c6343ec7594ca3abe930
Goodwin126 <gulindimon@mail.ru>
2024-06-15 01:15:40 +0300

    secondaryt commit

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -51,7 +51,7 @@
       class="m-auto w-14 h-14 focus:outline-none transition transform hover:scale-105 hover:-translate-y-1"
       src="/study.png"
     />
-    <imggit
+    <img
       status
       class="m-auto w-14 h-14 focus:outline-none transition transform hover:scale-105 hover:-translate-y-1"
       src="/awards.png"
