97c5e67d390a7a7c85a0e79f709ef05d99fd3e65
Goodwin126 <gulindimon@mail.ru>
2024-06-15 14:11:05 +0300

    secondaryt commit

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,9 +4,11 @@
       <div class="flex grid grid-cols-3">
         <img class="flex-justify-left m-auto" src="/Ellipse 3.png" />
         <div class="flex flex-col m-auto mt-2.5 ml-0">
-          <p class="text-lg text-white hyphens-none">Вода 1,5 л</p>
+          <p class="text-lg text-white overflow-auto touch-none select-none">Вода 1,5 л</p>
           <img class="m-auto ml-0" src="/Runner_4.png" />
-          <p class="text-xs text-gray-400 hyphens-none">рекомендовано 2,3 л</p>
+          <p class="text-xs text-gray-400 overflow-auto touch-none select-none">
+            рекомендовано 2,3 л
+          </p>
         </div>
         <img
           class="m-auto hover:cursor-pointer transition transform hover:scale-105 isLarge:scale-90"
@@ -14,8 +16,10 @@
         />
         <img class="flex-justify-left m-auto" src="/Ellipse 4.png" />
         <div class="flex flex-col mt-2.5">
-          <p class="text-lg text-white">Завтрак</p>
-          <p class="text-xs text-gray-400">рекомендовано 410 ккал</p>
+          <p class="text-lg text-white overflow-auto touch-none select-none">Завтрак</p>
+          <p class="text-xs text-gray-400 overflow-auto touch-none select-none">
+            рекомендовано 410 ккал
+          </p>
         </div>
         <img
           class="m-auto hover:cursor-pointer transition transform hover:scale-105 isLarge:scale-90"
@@ -23,8 +27,10 @@
         />
         <img class="flex-justify-left m-auto" src="/Ellipse 2.png" />
         <div class="flex flex-col mt-2.5">
-          <p class="text-lg text-white">Обед</p>
-          <p class="text-xs text-gray-400">рекомендовано 540 ккал</p>
+          <p class="text-lg text-white overflow-auto touch-none select-none">Обед</p>
+          <p class="text-xs text-gray-400 overflow-auto touch-none select-none">
+            рекомендовано 540 ккал
+          </p>
         </div>
         <img
           class="m-auto hover:cursor-pointer transition transform hover:scale-105"
@@ -32,8 +38,10 @@
         />
         <img class="flex-justify-left m-auto" src="/Ellipse 5.png" />
         <div class="flex flex-col mt-2.5">
-          <p class="text-lg text-white">Ужин</p>
-          <p class="text-xs text-gray-400">рекомендовано 320 ккал</p>
+          <p class="text-lg text-white overflow-auto touch-none select-none">Ужин</p>
+          <p class="text-xs text-gray-400 overflow-auto touch-none select-none">
+            рекомендовано 320 ккал
+          </p>
         </div>
         <img
           class="m-auto hover:cursor-pointer transition transform hover:scale-105"
@@ -60,9 +68,25 @@
       class="m-auto w-14 h-14 focus:outline-none transition transform hover:scale-105 hover:-translate-y-1"
       src="/results.png"
     />
-    <p class="m-auto text-base font-bold text-black hover:text-blue-800">Дневник</p>
-    <p class="m-auto text-base font-bold text-black hover:text-blue-800">Изучить</p>
-    <p class="m-auto text-base font-bold text-black hover:text-blue-800">Награды</p>
-    <p class="m-auto text-base font-bold text-black hover:text-blue-800">Результаты</p>
+    <p
+      class="m-auto text-base font-bold text-black hover:text-blue-800 overflow-auto touch-none select-none"
+    >
+      Дневник
+    </p>
+    <p
+      class="m-auto text-base font-bold text-black hover:text-blue-800 overflow-auto touch-none select-none"
+    >
+      Изучить
+    </p>
+    <p
+      class="m-auto text-base font-bold text-black hover:text-blue-800 overflow-auto touch-none select-none"
+    >
+      Награды
+    </p>
+    <p
+      class="m-auto text-base font-bold text-black hover:text-blue-800 overflow-auto touch-none select-none"
+    >
+      Результаты
+    </p>
   </div>
 </template>
