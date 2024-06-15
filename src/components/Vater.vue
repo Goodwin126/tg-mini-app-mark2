993b8a8f6c1d6d2c464f0cfffb8e1427c10f4dbf
Goodwin126 <gulindimon@mail.ru>
2024-06-15 21:57:37 +0300

    secondaryt commit

--- src/components/Vater.vue
+++ src/components/Vater.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col">
     <div class="flex text-3xl text-white place-content-center">Вода</div>
-    <div class="grid grid-cols-4 place-content-center bg-white mt-5 rounded-3xl m-10">
-      <img src="/youself_ml.jpg" alt="Свой вариант" class="w-3/6 m-auto" />
-      <img src="/50_ml.jpg" alt="50_мл" class="w-3/6 m-auto" />
-      <img src="/100_ml.jpg" alt="50_мл" class="w-3/6 m-auto" />
-      <img src="/200_ml.jpg" alt="50_мл" class="w-3/6 m-auto" />
+    <div class="grid grid-cols-4 place-content-center bg-white mt-5 rounded-xl m-10">
+      <img src="/youself_ml.jpg" alt="Свой вариант" class="w-3/6 m-auto mt-5" />
+      <img src="/50_ml.jpg" alt="50_мл" class="w-3/6 m-auto mt-5" />
+      <img src="/100_ml.jpg" alt="50_мл" class="w-3/6 m-auto mt-5" />
+      <img src="/200_ml.jpg" alt="50_мл" class="w-3/6 m-auto mt-5" />
       <p class="text-center font-bold text-blue-900">Свой вариант</p>
       <p class="text-center font-bold text-blue-900">50 мл</p>
       <p class="text-center font-bold text-blue-900">100 мл</p>
