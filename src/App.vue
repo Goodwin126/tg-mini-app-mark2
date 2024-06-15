c352bb3203cf825dcaa7fa39d57f5171caeb8939
Goodwin126 <gulindimon@mail.ru>
2024-06-15 23:12:31 +0300

    secondaryt commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script setup>
+import { ref } from 'vue'
+
 import Header from './components/Header.vue'
-import Menu from './components/Menu.vue'
+//import Menu from './components/Menu.vue'
 import Water from './components/Vater.vue'
+
+const isShrunk = ref({})
+
+const shrinkButton = (buttonId) => {
+  isShrunk.value[buttonId] = true
+  setTimeout(() => {
+    isShrunk.value[buttonId] = false
+  }, 100) // Задержка для анимации (в миллисекундах)
+}
 </script>
 
 <template>
   <div class="flex-col m-4">
     <Header />
     <div class="place-content-center mt-10 touch-none select-none">
-      <Water />
+      <Water :shrinkButton="shrinkButton" :isShrunk="isShrunk" />
       <!-- <Menu /> -->
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style>
+.shrunk {
+  transform: scale(0.9);
+  transition: transform 0.1s ease;
+}
+</style>
