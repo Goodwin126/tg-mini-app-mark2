255c41d1cda57f1306c07939f62621ba7571302a
Goodwin126 <gulindimon@mail.ru>
2024-06-16 15:45:22 +0300

    secondaryt commit

--- src/components/InputWater.vue
+++ src/components/InputWater.vue
@@ -1,8 +1,11 @@
 <template>
-  <p class="flex text-3xl font-bold text-blue-900 place-content-center">Введите ваш вариант</p>
+  <div class="flex place-content-center gap-2 mt-5">
+    <p class="flex text-3xl font-bold text-blue-900 place-content-center">Введите ваш вариант</p>
+    <img src="/a_bottle_of_water.jpg" alt="Bottle_of_water" class="w-10 h-10 bg-white rounded-xl" />
+  </div>
   <input
     class="mt-5 bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
     type="text"
-    placeholder="Ввведите воду в миллилитрах"
+    placeholder="Введите количество в миллилитрах"
   />
 </template>
