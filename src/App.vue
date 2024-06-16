a029de6bced7501205ec3af25d1827b7f0fcdd5a
Goodwin126 <gulindimon@mail.ru>
2024-06-16 17:41:35 +0300

    secondaryt commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Eating from './components/Eating.vue'
 import Header from './components/Header.vue'
 </script>
 
@@ -6,7 +7,8 @@ import Header from './components/Header.vue'
   <div class="flex-col m-4">
     <Header />
     <div class="place-content-center mt-10 touch-none select-none">
-      <router-view></router-view>
+      <Eating />
+      <!-- <router-view></router-view> -->
     </div>
   </div>
 </template>
---
+++ src/components/Eating.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col place-content-center gap-2">
+    <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy">
+      <div class="flex rounded-md p-1 bg-ready">
+        <p class="text-xl text-white p-1 m-auto">Готово</p>
+      </div>
+      <p class="flex text-xl font-bold text-blue-900 m-auto text-gray-200">ПРИЕМ ПИЩИ</p>
+      <div class="flex rounded-3xl p-3 bg-ready">
+        <p class="text-xl text-white m-auto">0</p>
+      </div>
+    </div>
+    <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy gap-1">
+      <div class="flex rounded-md p-1 bg-ready">
+        <p class="text-xl text-white p-1 m-auto">Продукты</p>
+      </div>
+      <div class="flex rounded-md p-1 bg-ready">
+        <p class="text-xl text-white p-1 m-auto">Избранное</p>
+      </div>
+      <div class="flex rounded-md p-1 bg-ready">
+        <p class="text-xl text-white p-1 m-auto">Блюда</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-navy {
+  background-color: rgb(5, 5, 27);
+}
+.bg-ready {
+  background-color: rgb(15, 15, 48);
+}
+</style>
