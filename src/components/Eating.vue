65971147669df9f939914e3a6100f056a319634e
Goodwin126 <gulindimon@mail.ru>
2024-06-16 17:47:11 +0300

    secondaryt commit

--- src/components/Eating.vue
+++ src/components/Eating.vue
@@ -2,11 +2,11 @@
   <div class="flex flex-col place-content-center gap-1">
     <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy">
       <div class="flex rounded-md p-1 bg-ready">
-        <p class="text-xs text-white p-1 m-auto">Готово</p>
+        <p class="text-sm font-bold text-white p-1 m-auto">Готово</p>
       </div>
-      <p class="flex text-xs font-bold text-blue-900 m-auto text-gray-200">ПРИЕМ ПИЩИ</p>
+      <p class="flex text-sm font-bold text-blue-900 m-auto text-gray-200">ПРИЕМ ПИЩИ</p>
       <div class="flex rounded-3xl p-3 bg-ready">
-        <p class="text-xs text-white m-auto">0</p>
+        <p class="text-sm font-bold text-white m-auto">0</p>
       </div>
     </div>
     <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy gap-1">
