73923c1d99d69eb59ee998452d80d65ea6a4b76a
Goodwin126 <gulindimon@mail.ru>
2024-06-16 18:06:08 +0300

    secondaryt commit

--- src/components/Eating.vue
+++ src/components/Eating.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col place-content-center gap-1">
+  <div class="flex flex-col rounded-xl h-full bg-black p-1 gap-1">
     <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy">
-      <div class="flex rounded-md p-1 bg-ready">
+      <div class="flex rounded-md p-1 bg-gray">
         <p class="text-sm font-bold text-white p-1 m-auto">Готово</p>
       </div>
       <p class="flex text-sm font-bold text-blue-900 m-auto text-gray-200">ПРИЕМ ПИЩИ</p>
-      <div class="flex rounded-3xl p-3 bg-ready">
+      <div class="flex rounded-3xl p-3 bg-gray">
         <p class="text-sm font-bold text-white m-auto">0</p>
       </div>
     </div>
@@ -26,6 +26,9 @@
 <style scoped>
 .bg-navy {
   background-color: rgb(5, 5, 27);
+}
+.bg-gray {
+  background-color: rgb(25, 25, 75);
 }
 .bg-ready {
   background-color: rgb(15, 15, 48);
