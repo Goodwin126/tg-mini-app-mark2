996fb0c1160e43eda020f7cb7457d7b67ad6b99e
Goodwin126 <gulindimon@mail.ru>
2024-06-16 17:44:13 +0300

    secondaryt commit

--- src/components/Eating.vue
+++ src/components/Eating.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="flex flex-col place-content-center gap-2">
+  <div class="flex flex-col place-content-center gap-1">
     <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy">
       <div class="flex rounded-md p-1 bg-ready">
-        <p class="text-xl text-white p-1 m-auto">Готово</p>
+        <p class="text-xs text-white p-1 m-auto">Готово</p>
       </div>
-      <p class="flex text-xl font-bold text-blue-900 m-auto text-gray-200">ПРИЕМ ПИЩИ</p>
+      <p class="flex text-xs font-bold text-blue-900 m-auto text-gray-200">ПРИЕМ ПИЩИ</p>
       <div class="flex rounded-3xl p-3 bg-ready">
-        <p class="text-xl text-white m-auto">0</p>
+        <p class="text-xs text-white m-auto">0</p>
       </div>
     </div>
     <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy gap-1">
       <div class="flex rounded-md p-1 bg-ready">
-        <p class="text-xl text-white p-1 m-auto">Продукты</p>
+        <p class="text-xs text-white p-1 m-auto">Продукты</p>
       </div>
       <div class="flex rounded-md p-1 bg-ready">
-        <p class="text-xl text-white p-1 m-auto">Избранное</p>
+        <p class="text-xs text-white p-1 m-auto">Избранное</p>
       </div>
       <div class="flex rounded-md p-1 bg-ready">
-        <p class="text-xl text-white p-1 m-auto">Блюда</p>
+        <p class="text-xs text-white p-1 m-auto">Блюда</p>
       </div>
     </div>
   </div>
