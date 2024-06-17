74c9b42d9fea1781dd2c46229d652b76c2b9ae4b
Goodwin126 <gulindimon@mail.ru>
2024-06-17 23:53:34 +0300

    secondaryt commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
   <div class="relative touch-none select-none">
-    <dive class="flex-col gap-2 hover:cursor-pointer absolute ml-4 mt-4">
+    <dive class="flex-col gap-2 hover:cursor-pointer absolute ml-8 mt-8">
       <img class="transition transform hover:scale-110 translate-y-1" src="/Rectangle 1.png" />
       <img class="mt-2 transition transform hover:scale-110 translate-y-1" src="/Rectangle 2.png" />
       <img class="mt-2 transition transform hover:scale-110 translate-y-1" src="/Rectangle 3.png" />
@@ -9,24 +9,24 @@
 
     <RouterLink to="/"
       ><div class="w-full">
-        <h1 class="text-xl font-bold text-gray-500 text-center">Сегодня: суббота</h1>
+        <h1 class="text-xl font-bold text-gray-500 text-center mt-8">Сегодня: суббота</h1>
         <div class="flex place-content-center mt-5">
           <img class="absolute" src="/Ellipse 1.png" />
           <div class="mt-4 grid grid-cols-1">
             <div class="flex m-auto hover:cursor-pointer transition transform hover:scale-105">
-              <img class="w-2,5 h-2,5 m-auto mr-4" src="/Ellipse 6.png" />
+              <img class="w-2,5 h-2,5 m-auto mr-8" src="/Ellipse 6.png" />
               <p class="ml-1 text-white font-bold">30% Жиры</p>
             </div>
             <div class="flex m-auto hover:cursor-pointer transition transform hover:scale-105">
-              <img class="w-2,5 h-2,5 m-auto mr-4" src="/Ellipse 7.png" />
+              <img class="w-2,5 h-2,5 m-auto mr-8" src="/Ellipse 7.png" />
               <p class="flex ml-1 text-blue-600 font-bold">30% Белки</p>
             </div>
             <div class="flex m-auto hover:cursor-pointer transition transform hover:scale-105">
-              <img class="w-2,5 h-2,5 m-auto mr-4" src="/Ellipse 8.png" />
+              <img class="w-2,5 h-2,5 m-auto mr-8" src="/Ellipse 8.png" />
               <p class="flexml-1 text-green-600 font-bold">30% Углеводы</p>
             </div>
             <div class="flex m-auto hover:cursor-pointer transition transform hover:scale-105">
-              <img class="w-5.5 h-5 m-auto mr-4" src="/water.png" />
+              <img class="w-5.5 h-5 m-auto mr-8" src="/water.png" />
               <p class="flexml-1 text-blue-300 font-bold">30% Воды</p>
             </div>
           </div>
