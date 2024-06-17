aed9e92e38f0c0ae9a5759bcd07b358b98caab13
Goodwin126 <gulindimon@mail.ru>
2024-06-18 00:40:43 +0300

    secondaryt commit

--- src/components/Eating.vue
+++ src/components/Eating.vue
@@ -3,30 +3,30 @@ import FoodsSearch from './FoodsSearch.vue'
 </script>
 <template>
   <div class="flex flex-col rounded-xl h-full bg-black p-1 gap-1">
-    <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy">
+    <div class="grid grid-cols-3 rounded-xl p-1 w-full bg-navy">
       <div class="flex rounded-md p-1 bg-gray">
         <p class="text-sm font-bold text-white p-1 m-auto">Готово</p>
       </div>
-      <p class="flex text-sm font-bold text-blue-900 m-auto text-gray-200">ПРИЕМ ПИЩИ</p>
-      <div class="flex rounded-3xl p-3 bg-gray">
-        <p class="text-sm font-bold text-white m-auto">0</p>
+      <p class="flex text-sm font-bold text-blue-900 m-auto text-gray-200 p-2">ПРИЕМ ПИЩИ</p>
+      <div class="flex rounded-2xl p-3 bg-gray">
+        <p class="text-sm font-bold text-white m-auto">0 ккал.</p>
       </div>
     </div>
-    <div class="grid grid-cols-3 rounded-xl p-2 w-full bg-navy gap-1">
+    <div class="grid grid-cols-3 rounded-xl p-1 w-full bg-navy gap-1">
       <div class="flex rounded-md p-1 bg-ready">
-        <p class="text-xs text-white p-1 m-auto">Продукты</p>
+        <p class="text-xs text-white p-2 m-auto">Продукты</p>
       </div>
       <div class="flex rounded-md p-1 bg-ready">
-        <p class="text-xs text-white p-1 m-auto">Избранное</p>
+        <p class="text-xs text-white p-2 m-auto">Избранное</p>
       </div>
       <div class="flex rounded-md p-1 bg-ready">
-        <p class="text-xs text-white p-1 m-auto">Блюда</p>
+        <p class="text-xs text-white p-2 m-auto">Блюда</p>
       </div>
     </div>
-    <div class="flex rounded-xl p-2 w-full bg-navy">
+    <div class="flex rounded-xl p-1 w-full bg-navy">
       <FoodsSearch />
     </div>
-    <div class="flex rounded-xl p-2 w-full bg-navy">
+    <div class="flex rounded-xl p-1 w-full bg-navy">
       <RouterLink to="/" class="w-full">
         <button class="text-white bg-gray rounded-lg text-sm font-bold text-center p-2.5 w-full">
           Назад
@@ -38,7 +38,7 @@ import FoodsSearch from './FoodsSearch.vue'
 
 <style scoped>
 .bg-navy {
-  background-color: rgb(11, 11, 51);
+  background-color: rgb(9, 9, 37);
 }
 .bg-gray {
   background-color: rgb(38, 38, 85);
