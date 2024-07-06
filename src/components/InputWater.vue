f2f4c447ce17ac0eb4c578dae2e5e87201ff4167
Goodwin126 <gulindimon@mail.ru>
2024-07-06 20:09:06 +0300

    new_version

--- src/components/InputWater.vue
+++ src/components/InputWater.vue
@@ -3,11 +3,10 @@
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black opacity-70 z-10"></div>
   <div class="fixed z-20 top-0 left-0 h-full w-full flex items-center justify-center">
-    <div class="flex flex-col bg-white rounded-3xl h-1/3 w-2/3 p-3">
-      <div class="flex flex-col gap-1">
-        <div class="flex flex-col">
-          <p class="flex text-xl font-bold text-blue-900 place-content-center">Ваш</p>
-          <p class="flex text-xl font-bold text-blue-900 place-content-center">вариант</p>
+    <div class="flex flex-col bg-white rounded-2xl h-2/6 w-2/3 p-5">
+      <div class="flex flex-col gap-3">
+        <div class="flex flex-col gap-2">
+          <p class="flex text-xl font-bold text-blue-900 place-content-center">Ваш вариант</p>
         </div>
         <div class="flex place-content-center flex-col w-full m-auto">
           <input
