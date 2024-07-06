7799ea30c63daad6b9df195b9664891ef7e59913
Goodwin126 <gulindimon@mail.ru>
2024-07-06 19:58:25 +0300

    new_version

--- src/components/InputWater.vue
+++ src/components/InputWater.vue
@@ -3,13 +3,13 @@
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black opacity-70 z-10"></div>
   <div class="fixed z-20 top-0 left-0 h-full w-full flex items-center justify-center">
-    <div class="flex flex-col bg-white rounded-3xl h-2/3 w-2/3 p-5">
+    <div class="flex flex-col bg-white rounded-3xl h-1/3 w-2/3 p-3">
       <div class="flex flex-col gap-1">
         <div class="flex flex-col">
-          <p class="flex text-xl font-bold text-blue-900 place-content-center">Введите ваш</p>
+          <p class="flex text-xl font-bold text-blue-900 place-content-center">Ваш</p>
           <p class="flex text-xl font-bold text-blue-900 place-content-center">вариант</p>
         </div>
-        <div class="flex place-content-center flex-col w-5/6 m-auto">
+        <div class="flex place-content-center flex-col w-full m-auto">
           <input
             class="no-spinner rounded-lg p-1 border-2 border-blue-800 w-full text-center"
             placeholder="Введите количество в мл."
